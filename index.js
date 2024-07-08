06c4d2f1eee2e1f84743ee451ffef684ff78ac63
asdwerte1 <samjmarkham@outlook.com>
2024-07-08 16:07:49 +0100

    Added underline animation to nav-bar

--- index.js
+++ index.js
@@ -25,4 +25,28 @@ function growIcon(element) {
 
 function shrinkIcon(element) {
     element.style.transform = "scale(1)";
-} 
+}
+
+$(document).ready(function () {
+    $(".nav-item").hover(function () {
+        const item = $(this);
+        const underline = $("<div class='underline'></div>").css({
+            position: "absolute",
+            bottom: "0",
+            left: "0",
+            height: "2px",
+            backgroundColor: "aquamarine",
+            width: "0",
+        });
+
+        item.css("position", "relative");
+        item.append(underline);
+        underline.animate({ width: "100%" }, fast);
+    },
+    function () {
+        const item = $(this);
+        item.find(".underline").animate({ width: "0" }, fast, function () {
+            $(this).remove();
+        });
+    });
+});
